a9ab614c470c76d1efa4385a30bc8b1d503f919f
David Araújo <david2araujo5@gmail.com>
2024-06-05 16:06:24 +0100

    refactor to lxc correct tool, not lxd wrapper

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,37 @@ use clap::{Args, Parser, Subcommand};
 )]
 struct CmtCli {
     #[command(subcommand)]
-    sub: Subcommands,
+    sub: Option<Subcommands>,
 
-    #[arg(short, long, help = "Show all information messages", global = true)]
-    verbose: bool,
+    #[arg(
+        short = 'o',
+        long,
+        value_name = "FILE",
+        help = "Output log to FILE instead of stderr",
+        global = true
+    )]
+    logfile: bool,
+
+    #[arg(
+        short,
+        long,
+        value_name = "LEVEL",
+        help = "Set log priority to LEVEL",
+        global = true
+    )]
+    logpriority: bool,
 
     #[arg(short, long, help = "Don't show progress information", global = true)]
     quiet: bool,
+
+    #[arg(
+        short = 'P',
+        long,
+        value_name = "PATH",
+        help = "Use specified container path",
+        global = true
+    )]
+    lxcpath: bool,
 }
 
 #[derive(Debug, Subcommand)]
@@ -46,44 +70,107 @@ enum Subcommands {
     visible_aliases = ["init"]
 )]
 struct CreateArgs {
-    #[arg(required = true)]
-    image: String,
-
-    #[arg(short, long, help = "Name for the new container")]
+    #[arg(short, long, help = "Name for the new container", required = true)]
     name: String,
 
-    #[arg(short, long, help = "Config key/value to apply to the new instance")]
+    #[arg(
+        short,
+        long,
+        value_name = "FILE",
+        help = "Config key/value to apply to the new instance"
+    )]
     config: Option<String>,
 
-    #[arg(short, long, help = "New key/value to apply to a specific device")]
-    device: Option<String>,
+    #[arg(
+        short,
+        long,
+        value_name = "TEMPLATE",
+        help = "Template to use to setup container",
+        required = true
+    )]
+    template: String,
 
-    #[arg(long, help = "Create an empty instance")]
-    empty: bool,
-
-    #[arg(short, long, help = "Ephemeral instance")]
-    ephemeral: bool,
+    #[command(subcommand)]
+    sub: Option<Backingstore>,
 
     #[arg(long, help = "Network name")]
     network: Option<String>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Backingstore {
+    DIR(DIRArgs),
+    LVM(LVMArgs),
+    ZFS(ZFSArgs),
+    LOOP(LOOPArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(
+    about,
+    long_about = "The container root filesystem will be a directory under /var/lib/lxc/container/rootfs"
+)]
+struct DIRArgs {
+    #[arg(long, value_name = "DIR", help = "Place rootfs directory under DIR")]
+    dir: Option<String>,
+}
+
+#[derive(Debug, Args)]
+#[command(about, long_about = "An lvm block device will be used")]
+struct LVMArgs {
+    #[arg(long, value_name = "LVNAME", help = "Use LVM lv name LVNAME")]
+    lvname: Option<String>,
 
     #[arg(
-        long = "no-profiles",
-        help = "Create the instance with no profiles applied"
+        long,
+        value_name = "VG",
+        default_value = "lxc",
+        help = "Use LVM in volume group called VG"
     )]
-    no_profiles: bool,
+    vgname: Option<String>,
 
-    #[arg(short, long, help = "Profile to apply to the new instance")]
-    profile: Option<String>,
+    #[arg(
+        long,
+        value_name = "TP",
+        default_value = "lxc",
+        help = "Use LVM thin pool called TP"
+    )]
+    thinpool: Option<String>,
+}
 
-    #[arg(short, long, help = "Storage pool name")]
-    storage: Option<String>,
+#[derive(Debug, Args)]
+#[command(
+    about,
+    long_about = "A complete ZFS filesystem will be created for the container"
+)]
+struct ZFSArgs {
+    #[arg(
+        long,
+        value_name = "PATH",
+        default_value = "tank/lxc",
+        help = "Create zfs under given zfsroot"
+    )]
+    zfsroot: Option<String>,
+}
 
-    #[arg(long, help = "Cluster member name")]
-    target: Option<String>,
+#[derive(Debug, Args)]
+#[command(about, long_about = "Can set type and size in a lvm block")]
+struct LOOPArgs {
+    #[arg(
+        long,
+        value_name = "TYPE",
+        default_value = "ext4",
+        help = "Create fstype TYPE"
+    )]
+    fstype: Option<String>,
 
-    #[arg(short, long, help = "Instance type")]
-    instance_type: Option<String>,
+    #[arg(
+        long,
+        value_name = "SIZE[U]",
+        default_value = "1G",
+        help = "Create filesystem of size SIZE * unit U (b|B|k|Km|Mg|G|t|T)"
+    )]
+    fssize: Option<String>,
 }
 
 #[derive(Debug, clap::Args)]
@@ -95,62 +182,58 @@ struct CreateArgs {
 )]
 struct DeleteArgs {
     #[arg(
-        value_delimiter=',',
-        value_names=["instance","[<instance>,....]"],
-        help = "Name of containers to delete"
+        value_delimiter = ',',
+        value_name = "NAME",
+        help = "Name of containers to delete [comma-separated]"
     )]
-    id: String,
+    name: Vec<String>,
+
+    #[arg(short, long, help = "destroy including all snapshots")]
+    snapshots: bool,
 
     #[arg(short, long, help = "Force the removal of running instances")]
     force: bool,
 
-    #[arg(short, long, help = "Require user confirmation")]
-    interactive: bool,
+    #[arg(long, value_name = "FILE", help = "Load configuration file FILE")]
+    rcfile: bool,
 }
 
 #[derive(Debug, clap::Args)]
-#[command(version, about, long_about = "Execute commands in containers")]
+#[command(
+    version,
+    about,
+    long_about = "Execute commands in containers",
+    visible_aliases = ["execute"])]
 struct ExecArgs {
-    #[arg(required=true)]
-    command: String,
+    #[arg(
+        required=true,
+        value_name="NAME",
+        help="NAME of the container"
+    )]
+    name: String,
 
-    #[arg(long, help = "Directory to run the command in (default /root)")]
+    #[arg(
+        required = true,
+        help="COMMAND to execute into this container")]
+    command: String,
+    
+    #[arg(short, long, help = "Daemonize the container")]
+    daemon: bool,
+
+    #[arg(long, default_value="/root", help = "Directory to run the command in")]
     cwd: Option<String>,
 
-    #[arg(short = 'n', long, help = "Disable stdin (reads from /dev/null)")]
-    disable_stdin: bool,
+    #[arg(short, long, value_delimiter = ',', help = "Environment variable(s) to set [e.g. HOME=/home/foo][comma-separated]")]
+    env: Option<Vec<String>>,
 
-    #[arg(
-        long,
-        help = "Environment variable to set (e.g. HOME=/home/foo)"
-    )]
-    env: Option<String>,
+    #[arg(long, value_name="FILE", help = "Environment variable FILE")]
+    envfile: Option<String>,
 
-    #[arg(
-        short = 't',
-        long = "force-interactive",
-        help = "Force pseudo-terminal allocation"
-    )]
-    force_interactive: bool,
+    #[arg(short, long, default_value="0", help = "User ID to run the command as")]
+    uid: Option<String>,
 
-    #[arg(
-        short = 'T',
-        long = "force-noninteractive",
-        help = "Disable pseudo-terminal allocation"
-    )]
-    force_noninteractive: bool,
-
-    #[arg(long, help = "Group ID to run the command as (default 0)")]
-    group: Option<String>,
-
-    #[arg(
-        long,
-        help = "Override the terminal mode (auto, interactive or non-interactive) (default \"auto\")"
-    )]
-    mode: Option<String>,
-
-    #[arg(long, help = "User ID to run the command as (default 0)")]
-    user: Option<String>,
+    #[arg(short, long, default_value="0", help = "Group ID to run the command as")]
+    gid: Option<String>,
 }
 
 fn main() {

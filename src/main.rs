8c6e71e58f73d775d4bba226510f5d8d2a4c293c
David Araújo <david2araujo5@gmail.com>
2024-06-11 23:04:58 +0100

    Expanded cli with command building

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod manage;
+mod utils;
+
 use clap::{Args, Parser, Subcommand};
+use std::{fmt::format, sync::atomic::Ordering};
+
+#[macro_use]
+extern crate lazy_static;
 
 #[derive(Debug, Parser)] // requires `derive` feature
 #[command(
@@ -31,7 +38,7 @@ struct CmtCli {
         help = "Output log to FILE instead of stderr",
         global = true
     )]
-    logfile: bool,
+    logfile: Option<String>,
 
     #[arg(
         short,
@@ -40,7 +47,7 @@ struct CmtCli {
         help = "Set log priority to LEVEL",
         global = true
     )]
-    logpriority: bool,
+    logpriority: Option<String>,
 
     #[arg(short, long, help = "Don't show progress information", global = true)]
     quiet: bool,
@@ -52,7 +59,7 @@ struct CmtCli {
         help = "Use specified container path",
         global = true
     )]
-    lxcpath: bool,
+    lxcpath: Option<String>,
 }
 
 #[derive(Debug, Subcommand)]
@@ -75,13 +82,23 @@ enum Subcommands {
     visible_aliases = ["init", "new"]
 )]
 struct CreateArgs {
-    #[arg(short, long, help = "Name for the new container", required = true)]
+    #[arg(help = "Name for the new container", required = true)]
     name: String,
 
-    #[arg(short, long, value_name = "FILE", help = "Config key/value to apply to the new instance")]
+    #[arg(
+        short,
+        long,
+        value_name = "FILE",
+        help = "Config key/value to apply to the new instance"
+    )]
     config: Option<String>,
 
-    #[arg(short, long, value_name = "TEMPLATE", help = "Template to use to setup container", required = true)]
+    #[arg(
+        short,
+        long,
+        value_name = "TEMPLATE",
+        help = "Template to use to setup container"
+    )]
     template: String,
 
     #[command(subcommand)]
@@ -95,6 +112,7 @@ struct CreateArgs {
 enum Backingstore {
     DIR(DIRArgs),
     LVM(LVMArgs),
+    RBD(RBDArgs),
     ZFS(ZFSArgs),
     LOOP(LOOPArgs),
 }
@@ -105,7 +123,11 @@ enum Backingstore {
     long_about = "The container root filesystem will be a directory"
 )]
 struct DIRArgs {
-    #[arg(value_name = "DIR", default_value = "/var/lib/lxc/container/rootfs", help = "Place rootfs directory under DIR")]
+    #[arg(
+        value_name = "DIR",
+        default_value = "/var/lib/lxc/container/rootfs",
+        help = "Place rootfs directory under DIR"
+    )]
     dir: String,
 }
 
@@ -115,11 +137,52 @@ struct LVMArgs {
     #[arg(long, value_name = "LVNAME", help = "Use LVM lv name LVNAME")]
     lvname: Option<String>,
 
-    #[arg(long, value_name = "VG", default_value = "lxc", help = "Use LVM in volume group called VG")]
+    #[arg(
+        long,
+        value_name = "VG",
+        default_value = "lxc",
+        help = "Use LVM in volume group called VG"
+    )]
     vgname: Option<String>,
 
-    #[arg(long, value_name = "TP", default_value = "lxc", help = "Use LVM thin pool called TP")]
+    #[arg(
+        long,
+        value_name = "TP",
+        default_value = "lxc",
+        help = "Use LVM thin pool called TP"
+    )]
     thinpool: Option<String>,
+
+    #[arg(
+        long,
+        value_name = "TYPE",
+        default_value = "ext4",
+        help = "Create fstype TYPE"
+    )]
+    fstype: Option<String>,
+
+    #[arg(
+        long,
+        value_name = "SIZE[U]",
+        default_value = "1G",
+        help = "Create filesystem of size SIZE * unit U (b|B|k|Km|Mg|G|t|T)"
+    )]
+    fssize: Option<String>,
+}
+
+#[derive(Debug, Args)]
+#[command(about, long_about = "An lvm block device will be used")]
+struct RBDArgs {
+    #[arg(long, value_name = "RBDNAME", help = "Use Ceph RBD name RBDNAME")]
+    rbdname: Option<String>,
+
+    #[arg(
+        long,
+        value_name = "POOL",
+        default_value = "lxc",
+        help = "Use Ceph RBD pool name POOL"
+    )]
+    rbdpool: Option<String>,
 }
 
 #[derive(Debug, Args)]
@@ -128,17 +191,32 @@ struct LVMArgs {
     long_about = "A complete ZFS filesystem will be created for the container"
 )]
 struct ZFSArgs {
-    #[arg(long, value_name = "PATH", default_value = "tank/lxc", help = "Create zfs under given zfsroot")]
+    #[arg(
+        long,
+        value_name = "PATH",
+        default_value = "tank/lxc",
+        help = "Create zfs under given zfsroot"
+    )]
     zfsroot: Option<String>,
 }
 
 #[derive(Debug, Args)]
 #[command(about, long_about = "Can set type and size in a lvm block")]
 struct LOOPArgs {
-    #[arg(long, value_name = "TYPE", default_value = "ext4", help = "Create fstype TYPE")]
+    #[arg(
+        long,
+        value_name = "TYPE",
+        default_value = "ext4",
+        help = "Create fstype TYPE"
+    )]
     fstype: Option<String>,
 
-    #[arg(long, value_name = "SIZE[U]", default_value = "1G", help = "Create filesystem of size SIZE * unit U (b|B|k|Km|Mg|G|t|T)")]
+    #[arg(
+        long,
+        value_name = "SIZE[U]",
+        default_value = "1G",
+        help = "Create filesystem of size SIZE * unit U (b|B|k|Km|Mg|G|t|T)"
+    )]
     fssize: Option<String>,
 }
 
@@ -150,8 +228,12 @@ struct LOOPArgs {
     visible_aliases = ["rm", "destroy"]
 )]
 struct DeleteArgs {
-    #[arg(value_delimiter = ',', value_name = "NAME", help = "Name of containers to delete [comma-separated]")]
-    name: Vec<String>,
+    #[arg(
+        value_delimiter = ',',
+        value_name = "NAME",
+        help = "Name of containers to delete"
+    )]
+    name: String,
 
     #[arg(short, long, help = "destroy including all snapshots")]
     snapshots: bool,
@@ -179,19 +261,38 @@ struct ExecuteArgs {
     #[arg(short, long, help = "Daemonize the container")]
     daemon: bool,
 
-    #[arg(long, default_value = "/root", help = "Directory to run the command in")]
+    #[arg(
+        long,
+        default_value = "/root",
+        help = "Directory to run the command in"
+    )]
     cwd: Option<String>,
 
-    #[arg(short, long, value_delimiter = ',', help = "Environment variable(s) to set [e.g. HOME=/home/foo][comma-separated]")]
+    #[arg(
+        short,
+        long,
+        value_delimiter = ',',
+        help = "Environment variable(s) to set [e.g. HOME=/home/foo][comma-separated]"
+    )]
     env: Option<Vec<String>>,
 
     #[arg(long, value_name = "FILE", help = "Environment variable FILE")]
     envfile: Option<String>,
 
-    #[arg(short, long, default_value = "0", help = "User ID to run the command as")]
+    #[arg(
+        short,
+        long,
+        default_value = "0",
+        help = "User ID to run the command as"
+    )]
     uid: Option<String>,
 
-    #[arg(short, long, default_value = "0", help = "Group ID to run the command as")]
+    #[arg(
+        short,
+        long,
+        default_value = "0",
+        help = "Group ID to run the command as"
+    )]
     gid: Option<String>,
 }
 
@@ -209,37 +310,81 @@ struct StartArgs {
     #[arg(short, long, help = "Daemonize the container (default)")]
     daemon: bool,
 
-    #[arg(short = 'F', long, help = "Start with the current tty attached to /dev/console")]
+    #[arg(
+        short = 'F',
+        long,
+        help = "Start with the current tty attached to /dev/console"
+    )]
     foreground: bool,
 
-    #[arg(short, long, value_name = "FILE", help = "Create a file with the process id")]
+    #[arg(
+        short,
+        long,
+        value_name = "FILE",
+        help = "Create a file with the process id"
+    )]
     pidfile: Option<String>,
 
     #[arg(long, value_name = "FILE", help = "Load configuration file FILE")]
     rcfile: Option<String>,
 
-    #[arg(short, long, value_name = "FILE", help = "Use specified FILE for the container console")]
+    #[arg(
+        short,
+        long,
+        value_name = "FILE",
+        help = "Use specified FILE for the container console"
+    )]
     console: Option<String>,
 
-    #[arg(short = 'L', long, value_name = "FILE", help = "Log container console output to FILE")]
+    #[arg(
+        short = 'L',
+        long,
+        value_name = "FILE",
+        help = "Log container console output to FILE"
+    )]
     console_log: Option<String>,
 
-    #[arg(short = 'C', long, help = "If any fds are inherited, close them (Note: --daemon implies --close-all-fds)")]
+    #[arg(
+        short = 'C',
+        long,
+        help = "If any fds are inherited, close them (Note: --daemon implies --close-all-fds)"
+    )]
     close_all_fds: bool,
 
-    #[arg(short = 's', long, value_name = "KEY=VAL", help = "Assign VAL to configuration variable KEY")]
+    #[arg(
+        short = 's',
+        long,
+        value_name = "KEY=VAL",
+        help = "Assign VAL to configuration variable KEY"
+    )]
     define: Option<String>,
 
-    #[arg(long, value_name = "NAME", help = "Share a network namespace with another container or pid")]
+    #[arg(
+        long,
+        value_name = "NAME",
+        help = "Share a network namespace with another container or pid"
+    )]
     share_net: Option<String>,
 
-    #[arg(long, value_name = "NAME", help = "Share an IPC namespace with another container or pid")]
+    #[arg(
+        long,
+        value_name = "NAME",
+        help = "Share an IPC namespace with another container or pid"
+    )]
     share_ipc: Option<String>,
 
-    #[arg(long, value_name = "NAME", help = "Share a UTS namespace with another container or pid")]
+    #[arg(
+        long,
+        value_name = "NAME",
+        help = "Share a UTS namespace with another container or pid"
+    )]
     share_uts: Option<String>,
 
-    #[arg(long, value_name = "NAME", help = "Share a PID namespace with another container or pid")]
+    #[arg(
+        long,
+        value_name = "NAME",
+        help = "Share a PID namespace with another container or pid"
+    )]
     share_pid: Option<String>,
 }
 
@@ -251,25 +396,47 @@ struct StartArgs {
     visible_aliases = ["halt", "terminate"]
 )]
 struct StopArgs {
-    #[arg(short, long, value_name = "NAME", help = "NAME of the container", required = true)]
+    #[arg(
+        short,
+        long,
+        value_name = "NAME",
+        help = "NAME of the container",
+        required = true
+    )]
     name: String,
 
     #[arg(short, long, help = "Reboot the container")]
     reboot: bool,
 
-    #[arg(short = 'W', long, help = "Don't wait for shutdown or reboot to complete")]
+    #[arg(
+        short = 'W',
+        long,
+        help = "Don't wait for shutdown or reboot to complete"
+    )]
     nowait: bool,
 
-    #[arg(short, long, value_name = "T", help = "Wait T seconds before hard-stopping")]
+    #[arg(
+        short,
+        long,
+        value_name = "T",
+        help = "Wait T seconds before hard-stopping"
+    )]
     timeout: Option<u64>,
 
-    #[arg(short, long,help = "Kill container rather than request clean shutdown")]
+    #[arg(
+        short,
+        long,
+        help = "Kill container rather than request clean shutdown"
+    )]
     kill: bool,
 
     #[arg(long, help = "Avoid using API locks")]
     nolock: bool,
 
-    #[arg(long, help = "Only request clean shutdown, don't force kill after timeout")]
+    #[arg(
+        long,
+        help = "Only request clean shutdown, don't force kill after timeout"
+    )]
     nokill: bool,
 
     #[arg(long, value_name = "FILE", help = "Load configuration file FILE")]
@@ -290,7 +457,12 @@ struct ListArgs {
     #[arg(short, long, help = "Use a fancy, column-based output")]
     fancy: bool,
 
-    #[arg(short = 'F', long, value_name = "COLUMNS", help = "Comma separated list of columns to show in the fancy output (valid columns: NAME, STATE, PID, RAM, SWAP, AUTOSTART, GROUPS, INTERFACE, IPV4 and IPV6, UNPRIVILEGED)")]
+    #[arg(
+        short = 'F',
+        long,
+        value_name = "COLUMNS",
+        help = "Comma separated list of columns to show in the fancy output (valid columns: NAME, STATE, PID, RAM, SWAP, AUTOSTART, GROUPS, INTERFACE, IPV4 and IPV6, UNPRIVILEGED)"
+    )]
     fancy_format: Option<String>,
 
     #[arg(long, help = "List only active containers")]
@@ -308,13 +480,27 @@ struct ListArgs {
     #[arg(long, help = "List only defined containers")]
     defined: bool,
 
-    #[arg(long, value_name = "NUM", help = "List nested containers up to NUM levels of nesting (default is 5)")]
+    #[arg(
+        long,
+        value_name = "NUM",
+        help = "List nested containers up to NUM levels of nesting (default is 5)"
+    )]
     nesting: Option<u32>,
 
-    #[arg(long, value_name = "REGEX", help = "Filter container names by regular expression")]
+    #[arg(
+        long,
+        value_name = "REGEX",
+        help = "Filter container names by regular expression"
+    )]
     filter: Option<String>,
 
-    #[arg(short, long, value_name = "GROUPS", value_delimiter = ',', help = "Comma separated list of groups a container must have to be displayed")]
+    #[arg(
+        short,
+        long,
+        value_name = "GROUPS",
+        value_delimiter = ',',
+        help = "Comma separated list of groups a container must have to be displayed"
+    )]
     groups: Option<Vec<String>>,
 }
 
@@ -326,10 +512,16 @@ struct ListArgs {
     visible_aliases = ["cp"]
 )]
 struct CopyArgs {
-    #[arg(value_name = "[CONTAINER]:SRC_PATH", help = "Source path (can be from the local host or from a container)")]
+    #[arg(
+        value_name = "[CONTAINER]:SRC_PATH",
+        help = "Source path (can be from the local host or from a container)"
+    )]
     source: String,
 
-    #[arg(value_name = "[CONTAINER]:DEST_PATH", help = "Destination path (can be to the local host or to a container)")]
+    #[arg(
+        value_name = "[CONTAINER]:DEST_PATH",
+        help = "Destination path (can be to the local host or to a container)"
+    )]
     destination: String,
 
     #[arg(short, long, help = "Archive mode (copy all uid/gid information)")]
@@ -338,7 +530,6 @@ struct CopyArgs {
     #[arg(short = 'L', long, help = "Always follow symbol link in SRC_PATH")]
     follow_link: Option<bool>,
 }
-
 
 #[derive(Debug, Args)]
 #[command(
@@ -354,10 +545,20 @@ struct ConfigArgs {
     #[arg(long, value_name = "FILE", help = "Load configuration file FILE")]
     rcfile: Option<String>,
 
-    #[arg(long, value_delimiter = ',', value_name = "value", help = "Value of a state object (for example, 'cpuset.cpus')")]
+    #[arg(
+        long,
+        value_delimiter = ',',
+        value_name = "value",
+        help = "Value of a state object (for example, 'cpuset.cpus')"
+    )]
     state_object: Option<Vec<String>>,
 
-    #[arg(short, long, value_name = "KEY", help = "Show configuration variable KEY from running container")]
+    #[arg(
+        short,
+        long,
+        value_name = "KEY",
+        help = "Show configuration variable KEY from running container"
+    )]
     config: Option<String>,
 
     #[arg(short, long, help = "Shows the IP addresses")]
@@ -379,17 +580,49 @@ struct ConfigArgs {
 fn main() {
     match CmtCli::try_parse() {
         Ok(cli) => {
-            println!("CLI arguments parsed successfully");
-            
+            quiet_println!("CLI arguments parsed successfully");
+
+            // Quiet mode suppresses all stdout
+            if cli.quiet {
+                utils::QUIET.store(true, Ordering::SeqCst);
+            }
+
+            // Command's global flags
+            let mut global_options: String = String::new();
+
+            if cli.logfile.is_some() {
+                global_options.push_str(&format!(" --logfile={}", cli.logfile.unwrap()));
+            }
+
+            if cli.logpriority.is_some() {
+                global_options.push_str(&format!(" --logpriority={}", cli.logpriority.unwrap()));
+            }
+
+            if cli.lxcpath.is_some() {
+                global_options.push_str(&format!(" --lxcpath={}", cli.lxcpath.unwrap()));
+            }
+
+            // Build command based on subcommands.
+            let mut cmdstr: String = String::new();
             match cli.sub {
-                // TODO: handle user input
-                Some(Subcommands::Start(_)) => println!("Start"),
-                Some(Subcommands::Delete(_)) => println!("Delete"),
-                _ => {},
+                Some(Subcommands::Create(args)) => cmdstr = manage::create(args),
+                Some(Subcommands::Delete(args)) => cmdstr = manage::delete(args),
+                Some(Subcommands::Execute(args)) => cmdstr = manage::execute(args),
+                Some(Subcommands::Start(args)) => cmdstr = manage::start(args),
+                Some(Subcommands::Stop(args)) => cmdstr = manage::stop(args),
+                Some(Subcommands::List(args)) => cmdstr = manage::list(args),
+                Some(Subcommands::Copy(args)) => cmdstr = manage::copy(args),
+                Some(Subcommands::Config(args)) => cmdstr = manage::config(args),
+                _ => {}
             };
-        },
+
+            // TODO: Execute command
+            quiet_println!("{:?}", cmdstr);
+            //let executable_command: String = format!();
+            //Exec::shell(executable_command);
+        }
         Err(e) => {
-            println!("Error parsing input! Please try again.\n");
+            quiet_println!("Error parsing input! Please try again.\n");
             _ = e.print();
         }
     }
--- src/manage.rs
+++ src/manage.rs
@@ -11,3 +11,170 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use crate::{
+    quiet_println, Backingstore, ConfigArgs, CopyArgs, CreateArgs, DeleteArgs, ExecuteArgs, ListArgs, StartArgs, StopArgs
+};
+use crate::utils;
+
+use core::sync::atomic::Ordering;
+use std::rc::Rc;
+
+pub fn create(args: CreateArgs) -> String {
+    let mut create_options: String = String::new();
+    if args.config.is_some() {
+        create_options.push_str(&format!(" --config={}", args.config.unwrap()));
+    }
+
+    // Backingstore modes
+    let mut backingstore_options: String = String::new();
+    match args.sub {
+        Some(Backingstore::DIR(o_args)) => {
+            backingstore_options.push_str(
+                &format!(" --dir={}",
+                o_args.dir));
+        },
+        Some(Backingstore::LVM(o_args)) => {
+            backingstore_options.push_str(
+                &format!(" --bdev lvm --lvname={} --vgname={} --thinpool={} --fssize={} --fstype={}",
+                o_args.lvname.unwrap_or(args.name.clone()),
+                o_args.vgname.unwrap(),
+                o_args.thinpool.unwrap(),
+                o_args.fssize.unwrap(),
+                o_args.fstype.unwrap()));
+        },
+        Some(Backingstore::RBD(o_args)) => {
+            backingstore_options.push_str(
+                &format!(" --bdev rbd --rbdname={} --rbdpool={}",
+                o_args.rbdname.unwrap_or(args.name.clone()),
+                o_args.rbdpool.unwrap()));
+        },
+        Some(Backingstore::ZFS(o_args)) => {
+            backingstore_options.push_str(
+                &format!(" --bdev=zfs --zfsroot={}",
+                o_args.zfsroot.unwrap()));
+        },
+        Some(Backingstore::LOOP(o_args)) => {
+            backingstore_options.push_str(
+                &format!(" --bdev=loop --fssize={} --fstype={}",
+                o_args.fssize.unwrap(),
+                o_args.fstype.unwrap()));
+        },
+        _ => {}
+    }
+
+    let cmdstr= format!(
+        "lxc-create --name={} --template={}{}{}",
+        args.name,
+        args.template,
+        create_options,
+        backingstore_options,
+    );
+
+    return cmdstr
+}
+
+pub fn delete(args: DeleteArgs) -> String {
+    let mut delete_options: String = String::new();
+
+    if args.force {
+        delete_options.push_str(&format!(" --force"));
+    }
+
+    if args.snapshots {
+        delete_options.push_str(&format!(" --snapshots"));
+    }
+
+    if args.rcfile.is_some() {
+        delete_options.push_str(&format!(" --rcfile={}", args.rcfile.unwrap()));
+    }
+    
+    let cmdstr= format!(
+        "lxc-destroy --name={}{}",
+        args.name,
+        delete_options,
+    );
+
+    return cmdstr;
+}
+
+pub fn execute(args: ExecuteArgs) -> String {
+    let mut execute_options: String = String::new();
+
+    if args.daemon {
+        execute_options.push_str(&format!(" --snapshots"));
+    }
+
+    let cmdstr= format!(
+        "lxc-execute --name={}{}",
+        args.name,
+        execute_options,
+    );
+
+    return cmdstr;
+}
+
+// TODO
+pub fn start(args: StartArgs) -> String{
+    let mut start_options: String = String::new();
+
+    let cmdstr= format!(
+        "lxc-start --name={}{}",
+        args.name,
+        start_options,
+    );
+
+    return cmdstr;
+}
+
+// TODO
+pub fn stop(args: StopArgs) -> String {
+    let mut stop_options: String = String::new();
+
+    let cmdstr= format!(
+        "lxc-stop --name={}{}",
+        args.name,
+        stop_options,
+    );
+
+    return cmdstr;
+}
+
+// TODO
+pub fn list(args: ListArgs) -> String {
+    let mut list_options: String = String::new();
+
+    let cmdstr= format!(
+        "lxc-list --name={}{}",
+        args.name,
+        list_options,
+    );
+
+    return cmdstr;
+}
+
+// TODO
+pub fn copy(args: CopyArgs) -> String {
+    let mut copy_options: String = String::new();
+
+    let cmdstr= format!(
+        "lxc-copy --name={}{}",
+        args.name,
+        copy_options,
+    );
+
+    return cmdstr;
+}
+
+// TODO
+pub fn config(args: ConfigArgs) -> String {
+    let mut config_options: String = String::new();
+
+    let cmdstr= format!(
+        "lxc-config --name={}{}",
+        args.name,
+        config_options,
+    );
+
+    return cmdstr;
+}
